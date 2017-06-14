b006ba0f83772804d9781d319aec040ec7465a6f
Yota Toyama <raviqqe@gmail.com>
2017-06-15 00:49:48 +0900

    Parse command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,35 @@
+extern crate docopt;
+#[macro_use]
+extern crate serde_derive;
+
 mod thunk;
 
-use thunk::{Thunk, Args};
+use thunk::Thunk;
+
+#[derive(Debug, Deserialize)]
+struct Args {
+    arg_filename: String,
+}
+
+fn get_args() -> Args {
+    let usage = "
+Tisp interpreter.
+
+Usage:
+    tisp [<filename>]
+    tisp (-h | --help)
+
+Options:
+    -h, --help  Show help.
+";
+
+    docopt::Docopt::new(usage)
+        .and_then(|d| d.deserialize())
+        .unwrap_or_else(|e| e.exit())
+}
 
 fn main() {
-    let t = Thunk::app(Thunk::num(123.0), Args::new());
+    let args = get_args();
+    let t = Thunk::app(Thunk::num(123.0), thunk::Args::new());
     println!("Hello, Tisp!");
 }
